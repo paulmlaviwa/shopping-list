af91fb17140fb437f1d6b0813312534481a92299
paulmlaviwa <mlaviwa.paul@gmail.com>
2023-10-04 11:30:52 -0400

    first commit

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args){
+        System.out.println("My shopping list: 1. Pens" +
+                "2. Calculator" +
+                "3. Plain Papers" +
+                "4. Books" +
+                "5. Headphones" +
+                "6. VGA Cable" +
+                "7. 26-inch monitor" +
+                "8. LED RGB Lights" +
+                "9. Pens organizer" +
+                "10. Water ");
+    }
+
+}
